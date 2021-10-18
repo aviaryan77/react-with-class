aedc87f514d6e09ae80b9dcea8fd5f8f93f409ff
avinasharyan614 <83573216+avinasharyan614@users.noreply.github.com>
2021-10-19 01:49:39 +0530

    first set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hi, I am react app
+      </h1>
+    </div>
+  );
+}
+
+export default App;
